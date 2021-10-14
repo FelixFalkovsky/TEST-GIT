42a08517d1d439cc7d4e861cca2f6d67890ebe6a
FelixFalkovsky <56316435+FelixFalkovsky@users.noreply.github.com>
2021-10-14 22:04:31 +0300

    init commit

---
+++ TEST-GIT/Shared/TEST_GITApp.swift
@@ -0,0 +1,17 @@
+//
+//  TEST_GITApp.swift
+//  Shared
+//
+//  Created by Felix on 14.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TEST_GITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
